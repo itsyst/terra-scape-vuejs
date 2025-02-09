0c2bb7e2f055c4ea2a31b4aa4cc1b95bec7d3dcf
Khaled El Hamzi <contact@elhamzi.me>
2025-02-09 21:33:22 +0100

    feat: add `Features` component and integrate `oh-vue-icons` library
    - Added a new `Features` component with dynamic icon rendering using the `oh-vue-icons` library.
    - Integrated `oh-vue-icons` into the project dependencies (`package.json`) to support custom SVG icons.
    - Updated the main application template to include the new `Features` component.
    - Enhanced the `Features` component with:
      - Dynamic rendering of feature cards based on an array of feature objects.
      - Icons for each feature card, including `BiGlobe`, `GiCompass`, `LaMapMarkedAltSolid`, `OiRocket`, and `RiMentalHealthLine`.
      - Styled the component using SCSS from `@/sass/components/_features`.
    - Ensured proper initialization of features data using Vue's `onMounted` lifecycle hook.
    
    BREAKING CHANGE: None

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Header/>
   <Tours/>
+  <Features/>
 </template>
 
 <script setup lang="ts">
 import Header from './components/header.vue';
 import Tours from './components/tours.vue';
+import Features from './components/features.vue';
 </script>
---
+++ src/components/features.vue
@@ -0,0 +1,99 @@
+<template>
+	<div class="feature">
+		<div class="feature__cards">
+			<div
+				v-for="(feature, index) in features"
+				:key="index"
+				class="feature__card"
+			>
+				<div class="feature__card--content">
+					<OhVueIcon
+						:name="feature.icon"
+						:fill="feature.fill"
+						:scale="feature.scale"
+					/>
+
+					<p class="feature__card--description">{{ feature.description }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { OhVueIcon, addIcons } from 'oh-vue-icons';
+	import {
+		BiGlobe,
+		GiCompass,
+		OiRocket,
+		LaMapMarkedAltSolid,
+		RiMentalHealthLine
+	} from 'oh-vue-icons/icons';
+	import { onMounted, ref } from 'vue';
+
+	const colorFill = ref('#f953c7');
+	interface Feature {
+		icon: string;
+		fill: string;
+		scale?: number;
+		description: string;
+	}
+	const features = ref<Feature[]>([]);
+
+	addIcons(
+		BiGlobe,
+		GiCompass,
+		LaMapMarkedAltSolid,
+		RiMentalHealthLine,
+		OiRocket
+	);
+
+	onMounted(async () => {
+		try {
+			// Update the tours array with dynamic image objects
+			features.value = [
+				{
+					icon: 'bi-globe',
+					fill: colorFill.value,
+					scale: 2,
+					description:
+						'Utforska världen med våra globala tjänster. Få tillgång till internationella resurser och information.'
+				},
+				{
+					icon: 'gi-compass',
+					fill: colorFill.value,
+					scale: 2.3,
+					description:
+						'Navigera enkelt med avancerade kompassverktyg. Hitta rätt väg var som helst i världen.'
+				},
+				{
+					icon: 'la-map-marked-alt-solid',
+					fill: colorFill.value,
+					scale: 2,
+					description:
+						'Peka ut platser exakt med detaljerade kartor. Lätta att använda för alla destinationer.'
+				},
+				{
+					icon: 'oi-rocket',
+					fill: colorFill.value,
+					scale: 2,
+					description:
+						'Driv framtidens innovationer. Uppnå nya höjder med vårt fortsatta teknologiska framsteg.'
+				},
+				{
+					icon: 'ri-mental-health-line',
+					fill: colorFill.value,
+					scale: 2,
+					description:
+						'Framtvinga mentalt välbefinnande genom våra hälsfokuserade initiativ. Stöd och hjälp när du behöver det.'
+				}
+			];
+		} catch (error) {
+			console.error((error as any).message);
+		}
+	});
+</script>
+
+<style lang="scss" scoped>
+	@use '@/sass/components/_features';
+</style>
