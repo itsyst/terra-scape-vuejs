074f5b3cd12d0612a131dd11443abce70f157552
Khaled El Hamzi <contact@elhamzi.me>
2025-02-19 13:26:58 +0100

    fix(build): resolve app.vue import path

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
 import './sass/main.scss';
-import App from './app.vue';
+import App from '@/app.vue'
 
 // Create app instance
-const app = createApp(App);
- 
-// Mount the app
-app.mount('#app');
+createApp(App).mount('#app')
